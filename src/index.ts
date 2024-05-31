c2537cdd31f994a639d7e808b6cff5964a29cb48
DWaveletT <dingwentao2004@126.com>
2024-05-31 15:51:25 +0800

    little fix

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export { default as CasketEditor } from './components/MEditor.vue';
 export { default as CasketViewer } from './components/MViewer.vue';
 
 export type { Tool, ToolGroup, Toolbar } from './components/MToolbar.vue';
-export type { Plugins, CasketView } from './CasketStar.vue';
+export type { Plugins, Uploader, CasketView } from './CasketStar.vue';
 
 export * from './components/dialog';
 
